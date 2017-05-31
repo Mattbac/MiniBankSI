58dccb1ec7dc3e58720ab65ec3878e662bdd6063
matthieu <matthieu.bacoup@gmail.com>
2017-05-31 13:31:10 +0200

    change IAccount

--- src/main/java/com/projet/dao/IAccountDAO.java
+++ src/main/java/com/projet/dao/IAccountDAO.java
@@ -1,11 +1,11 @@
 package com.projet.dao;
 
-import com.projet.entity.Account;
+import com.projet.entity.AbstractAccount;
 
 public interface IAccountDAO {
 
-	public Account findAccountById(long id);
+	public AbstractAccount findAccountById(long id);
 	public void createAccount();
-	public void updateAccount(Account account);
-	public void deleteAccount(Account account);
+	public void updateAccount(AbstractAccount account);
+	public void deleteAccount(AbstractAccount account);
 }
