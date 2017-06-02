dce4bb5cf4e9949642b68218d26f7167a815536d
matthieu <matthieu.bacoup@gmail.com>
2017-06-02 12:23:18 +0200

    generation db

--- src/main/java/com/projet/presentation/HomeController.java
+++ src/main/java/com/projet/presentation/HomeController.java
@@ -12,7 +12,12 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.projet.dao.ICounselorDAO;
+import com.projet.entity.Client;
 import com.projet.entity.Counselor;
+import com.projet.entity.CurrentAccount;
+import com.projet.entity.Manager;
+import com.projet.entity.Role;
+import com.projet.entity.SavingAccount;
 import com.projet.service.IClientService;
 import com.projet.service.UserSecurity;
 
@@ -28,6 +33,30 @@ public class HomeController {
 	
 	@RequestMapping(value = {"/", "/home"}, method = RequestMethod.GET)
 	public String home() {
+		
+		Role rc = new Role("ROLE_counselor");
+		Role rm = new Role("ROLE_manager");
+		
+		for(int i = 0; i < 2 ;i++){
+			Manager m = new Manager("manager"+i, "manager"+i, rm, "Agency "+i);
+			
+			int g = (int) Math.floor((Math.random() * 5) + 3);
+			
+			for(int j = 0; j < g ;j++){
+				Counselor c = new Counselor("counselor"+j, "counselor"+j, rc, m);
+				
+				int r = (int) Math.floor((Math.random() * 9) + 1);
+				
+				for(int k = 0; k < r ;k++){
+					
+					/*CurrentAccount current = new CurrentAccount();
+					SavingAccount saving = new SavingAccount();*/
+					
+					Client ct = new Client("firstname"+k, "lastname"+k, "adress", "zipcode", "city", "phonenumber", c);
+				}
+			}
+		}
+		
 		return "home";
 	}
 	
